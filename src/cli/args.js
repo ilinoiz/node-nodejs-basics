45c376cc28753e9fe2e05333a13279e77adc02f0
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-17 23:56:53 +0200

    feat: add cli folder tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = {};
+  for (let i = 2; i < process.argv.length - 1; i += 2) {
+    args[process.argv[i]] = process.argv[i + 1];
+  }
+
+  let result = "";
+  for (const arg in args) {
+    result += `${arg.replace("--", "")} is ${args[arg]}, `;
+  }
+  
+  console.log(result.substring(0, result.length - 2));
 };
 
 parseArgs();
