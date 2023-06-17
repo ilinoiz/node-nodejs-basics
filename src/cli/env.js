13f156763c76f731ca5bfc4ef34f0339b17cdea5
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-17 23:56:53 +0200

    feat: add cli folder tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssEnvVariable = "RSS_";
+  let result = "";
+
+  for (const key in process.env) {
+    if (key.startsWith(rssEnvVariable)) {
+      result += `${key}=${process.env[key]}; `;
+    }
+  }
+
+  console.log(result);
 };
 
 parseEnv();
