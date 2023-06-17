f2c3286e61727f33b793372c3efb289925e15907
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-18 00:07:52 +0200

    feat: add hash folder task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+import path from "path";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    "src",
+    "hash",
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const fileData = await fs.readFile(filePath, "utf8");
+  const hash = crypto.createHash("sha256");
+  hash.update(fileData);
+  
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();
