7ba54a021c1f615c5fea109681cbbf679f612b49
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-18 16:09:58 +0200

    feat: add streams task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
-const transform = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+import { Transform } from "stream";
+import { EOL } from "os";
+
+const myTransform = async () => {
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split("").reverse().join("").concat(EOL));
+    },
+  });
+
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
-await transform();
+await myTransform();
