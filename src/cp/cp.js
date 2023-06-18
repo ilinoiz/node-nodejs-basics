0467d660dc22f5ab6792fbfc5247cae34d2e85e9
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-18 19:42:14 +0200

    feat: add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,7 @@
+import { fork } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const forked = fork("src/cp/files/script.js", args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
