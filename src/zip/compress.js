0d3c5200b1298578adc577bb907decc637641dc2
Alena Yermolenka <ilinoiz.ilinoiz@mail.ru>
2023-06-19 11:36:19 +0200

    feat: add zip task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const basePath = path.join("src", "zip", "files");
+  const fileToCompressPath = path.join(basePath, "fileToCompress.txt");
+  const fileToWritePath = path.join(basePath, "archive.gz");
+  const readStream = fs.createReadStream(fileToCompressPath, {
+    encoding: "utf8",
+  });
+  const writeStream = fs.createWriteStream(fileToWritePath);
+
+  readStream.pipe(zlib.createGzip()).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+  const basePath = path.join("src", "zip", "files");
+  const toFilePath = path.join(basePath, "fileToCompress.txt");
+  const fromFilePath = path.join(basePath, "archive.gz");
+  const readStream = fs.createReadStream(fromFilePath);
+  const writeStream = fs.createWriteStream(toFilePath);
+
+  readStream.pipe(zlib.createGunzip()).pipe(writeStream);
 };
 
 await decompress();
